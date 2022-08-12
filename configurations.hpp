ed422ae90ff01d491fd795cb68315ee00b9254c9
Yehor Zubko <egorz1903@gmail.com>
2022-08-12 15:09:39 +0300

    inlined constants in configurations file

--- configurations.hpp
+++ configurations.hpp
@@ -4,18 +4,18 @@
 #include <string>
 
 // Discord bot token
-const std::string BOT_TOKEN = "-"; /* INSERT YOUR DISCORD BOT TOKEN HERE */
+inline const std::string BOT_TOKEN = "-"; /* INSERT YOUR DISCORD BOT TOKEN HERE */
 
 // Hypixel API key
-const std::string HYPIXEL_API_KEY = "-"; /* INSERT YOUR HYPIXEL API KEY HERE */
+inline const std::string HYPIXEL_API_KEY = "-"; /* INSERT YOUR HYPIXEL API KEY HERE */
 
 // How much users can use the bot per one API refresh cycle
-const size_t MAX_USERS = 120;
+inline const size_t MAX_USERS = 120;
 
 // Delay between the clearing of the recent users list in seconds
-const uint64_t RECENT_USERS_RESET_FREQUENCY = 120;
+inline const uint64_t RECENT_USERS_RESET_FREQUENCY = 120;
 
 // How much space should be reserved for stats roles in vectors
-const size_t ROLES_RESERVE = 50;
+inline const size_t ROLES_RESERVE = 50;
 
 #endif // BOT108_CONFIGURATIONS_HPP
